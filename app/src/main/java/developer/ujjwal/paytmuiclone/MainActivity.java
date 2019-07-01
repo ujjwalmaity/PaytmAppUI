ddb028f3cb2c5f877e5f7797c0ce664f81496faf
UJJWAL <ujjwalmaity98@gmail.com>
2019-07-01 16:54:16 +0530

    Bottom Navigation

--- app/src/main/java/developer/ujjwal/paytmuiclone/MainActivity.java
+++ app/src/main/java/developer/ujjwal/paytmuiclone/MainActivity.java
@@ -1,7 +1,11 @@
 package developer.ujjwal.paytmuiclone;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            selectFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_mall:
+                            selectFragment = new MallFragment();
+                            break;
+                        case R.id.nav_bank:
+                            selectFragment = new BankFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectFragment).commit();
+
+                    return true;
+                }
+            };
 }
